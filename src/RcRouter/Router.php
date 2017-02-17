49a795daee9c34f70f12d93c378a3f8ad0d19d17
Robert Cox <rcox@dontbebroke.com>
2017-02-17 12:02:19 -0800

    Base Structure and composer json in placce

---
+++ src/RcRouter/Router.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RcRouter;
+
+class Router
+{
+    public function __construct()
+    {
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
